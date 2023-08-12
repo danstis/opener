5f726bb5db134aa548fbd998991e538887cd4bc3
Dan Anstis <dan@bsod.co.nz>
2023-08-12 16:35:58 +1000

    aider: Added main entry point to main.py for building into a binary executable with pyinstaller.
    # Aider chat conversation:
    
    USER: update the main.py file to have the main entrypoint so it can built built into a binary executable with pyinstaller
    
    ASSISTANT: To make the `main.py` file executable, we need to add a main entry point. This is typically done by adding a `if __name__ == "__main__":` block at the end of the file. This block will only be executed if the file is run directly, not if it is imported as a module.
    
    Here is the change:
    
    ```python
    opener\main.py
    <<<<<<< HEAD
    import tkinter as tk
    import pystray
    from PIL import Image
    
    def create_menu(icon, item):
        if item.text == 'Exit':
            icon.stop()
    
    def setup(icon):
        icon.visible = True
    
    image_path = "img/can-opener.ico"
    image = Image.open(image_path)
    icon = pystray.Icon("name", image, "My System Tray Icon", menu=pystray.Menu(pystray.MenuItem('Version 0.1.0', lambda text: None), pystray.MenuItem('Exit', create_menu)))
    icon.run(setup)
    =======
    import tkinter as tk
    import pystray
    from PIL import Image
    
    def create_menu(icon, item):
        if item.text == 'Exit':
            icon.stop()
    
    def setup(icon):
        icon.visible = True
    
    def main():
        image_path = "img/can-opener.ico"
        image = Image.open(image_path)
        icon = pystray.Icon("name", image, "My System Tray Icon", menu=pystray.Menu(pystray.MenuItem('Version 0.1.0', lambda text: None), pystray.MenuItem('Exit', create_menu)))
        icon.run(setup)
    
    if __name__ == "__main__":
        main()
    >>>>>>> updated
    ```
    
    This change wraps the existing code in a new `main()` function, and then calls that function if the file is being run directly. This will allow PyInstaller to correctly build the file into an executable.

--- opener/main.py
+++ opener/main.py
@@ -9,7 +9,11 @@ def create_menu(icon, item):
 def setup(icon):
     icon.visible = True
 
-image_path = "img/can-opener.ico"
-image = Image.open(image_path)
-icon = pystray.Icon("name", image, "My System Tray Icon", menu=pystray.Menu(pystray.MenuItem('Version 0.1.0', lambda text: None), pystray.MenuItem('Exit', create_menu)))
-icon.run(setup)
+def main():
+    image_path = "img/can-opener.ico"
+    image = Image.open(image_path)
+    icon = pystray.Icon("name", image, "My System Tray Icon", menu=pystray.Menu(pystray.MenuItem('Version 0.1.0', lambda text: None), pystray.MenuItem('Exit', create_menu)))
+    icon.run(setup)
+
+if __name__ == "__main__":
+    main()
