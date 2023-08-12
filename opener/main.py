f5ae993a69a28d7ed63c200fcff47daf15bfacf7
Dan Anstis <dan@bsod.co.nz>
2023-08-12 16:34:49 +1000

    aider: Added opener/main.py

---
+++ opener/main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+import pystray
+from PIL import Image
+
+def create_menu(icon, item):
+    if item.text == 'Exit':
+        icon.stop()
+
+def setup(icon):
+    icon.visible = True
+
+image_path = "img/can-opener.ico"
+image = Image.open(image_path)
+icon = pystray.Icon("name", image, "My System Tray Icon", menu=pystray.Menu(pystray.MenuItem('Version 0.1.0', lambda text: None), pystray.MenuItem('Exit', create_menu)))
+icon.run(setup)
