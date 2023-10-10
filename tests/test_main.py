bff22852de6c5dcb6bef5af07f66ae926c2de8eb
Dan Anstis <dan.anstis@bdo.global>
2023-10-10 11:13:59 +1000

    tests: fix tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,33 +3,32 @@ import pytest
 from unittest import mock
 from opener import main
 
-def test_read_config(tmp_path):
-    # Create a temporary JSON file with some configuration data
-    config_data = {"image_path": "img/test-icon.ico"}
-    config_file = tmp_path / "config.json"
-    with open(config_file, "w") as f:
-        json.dump(config_data, f)
 
-    # Call read_config with the path to the temporary file
-    config = main.read_config(config_file)
+class TestMain:
+    def test_read_config(self, tmp_path):
+        # Create a temporary JSON file with some configuration data
+        config_data = {"image_path": "img/test-icon.ico"}
+        config_file = tmp_path / "config.json"
+        with open(config_file, "w") as f:
+            json.dump(config_data, f)
 
-    # Assert that the returned dictionary matches the configuration data
-    assert config == config_data
+        # Call read_config with the path to the temporary file
+        config = main.read_config(config_file)
 
-def test_create_menu():
-    # Test the create_menu function
-    # This is a placeholder test and may need to be updated based on the functionality of your create_menu function
-    icon = mock.Mock()
-    item = mock.Mock()
-    item.text = "Exit"
-    assert main.create_menu(icon, item) == None
+        # Assert that the returned dictionary matches the configuration data
+        assert config == config_data
 
-def test_setup():
-    # Test the setup function
-    # This is a placeholder test and may need to be updated based on the functionality of your setup function
-    assert main.setup(None) == None
+    def test_create_menu(self):
+        # Test the create_menu function
+        # This is a placeholder test and may need to be updated based on the functionality of your create_menu function
+        icon = mock.Mock()
+        item = mock.Mock()
+        item.text = "Exit"
+        assert main.create_menu(icon, item) == None
 
-def test_main():
-    # Test the main function
-    # This is a placeholder test and may need to be updated based on the functionality of your main function
-    assert main.main() == None
+    def test_setup(self):
+        # Test the setup function
+        # This is a placeholder test and may need to be updated based on the functionality of your setup function
+        icon = mock.Mock()
+        main.setup(icon)
+        assert icon.visible == True
