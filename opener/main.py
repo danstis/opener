0c2be6a8967b6cbb653ee45d3d2f64a6be256503
Dan Anstis <dan.anstis@bdo.global>
2023-10-10 12:02:36 +1000

    wip

--- opener/main.py
+++ opener/main.py
@@ -1,7 +1,14 @@
+import logging
+import time
 import tkinter as tk
 import pystray  # type: ignore
 import json
+import subprocess
 from PIL import Image
+
+
+_LOGGER = logging.getLogger(__name__)
+VERSION = "0.0.0-develop"
 
 
 def create_menu(icon, item) -> None:
@@ -30,36 +37,59 @@ def read_config(path: str = "config.json") -> dict:
     return config
 
 
-def setup(icon: pystray.Icon) -> None:
+def launch_apps() -> None:
     """
-    Sets the visibility of the given icon.
+    Launches the applications listed in the config.
+
+    Returns:
+        None
+    """
+    config = read_config()
+    for app in config["apps"]:
+        _LOGGER.info("- Launching %s", app["name"])
+        subprocess.Popen(app["command"])
+
+        # Wait for the application to launch
+        time.sleep(5)
+
+
+def setup_systray_menu_and_launch_apps(icon: pystray.Icon) -> None:
+    """
+    Sets up the systray menu and launches the applications.
 
     Parameters:
-        icon (Icon): The icon to set the visibility for.
+        icon (Icon): The icon to set up the systray menu for.
 
     Returns:
         None
     """
     icon.visible = True
 
+    _LOGGER.info("Launching applications")
+    launch_apps()
+
 
 def main() -> None:
     """
     Initializes the main function.
     """
+    _LOGGER.info("Reading configuration file")
     config = read_config()
+    _LOGGER.setLevel(config.get("logLevel", "INFO"))
+    _LOGGER.info("Creating systray menu")
     image_path = config.get("image_path", "img/can-opener.ico")
     image = Image.open(image_path)
     icon = pystray.Icon(
         "name",
         image,
-        "My System Tray Icon",
+        "Opener",
         menu=pystray.Menu(
-            pystray.MenuItem("Opener v0.1.0", lambda text: None),
+            pystray.MenuItem(f"Opener v{VERSION}", lambda text: None),
             pystray.MenuItem("Exit", create_menu),
         ),
     )
-    icon.run(setup)
+
+    icon.run(setup_systray_menu_and_launch_apps)
 
 
 if __name__ == "__main__":
